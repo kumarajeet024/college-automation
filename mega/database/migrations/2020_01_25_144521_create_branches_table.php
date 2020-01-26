09dec99d074980216cc1f0defc8b9db9a3035b18
isaty <66satyamsingh@gmail.com>
2020-01-26 18:22:22 +0530

    first commit

---
+++ mega/database/migrations/2020_01_25_144521_create_branches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('branch',50)->unique();
+            $table->string('branch_code',5);
+            $table->string('semesters');
+            $table->string('hod');
+            $table->string('course');
+            $table->string('branch_abr',10)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+}
